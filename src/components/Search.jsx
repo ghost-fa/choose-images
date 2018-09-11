8448459adc31f91241b9d8f54bae6b49e62eda16
ghost-fk <fayyad_r22@yahoo.com>
2018-09-11 09:43:02 +0200

    creat varibel val and set to e.target.value . and add if condtion if val equal nothing i set the images with empty array alse i put the axios

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -15,10 +15,16 @@ class Search extends Component{
   };
 
   onTextChange = (e) =>{
-    this.setState({[e.target.name]:e.target.value}, () =>{
-      axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`).then(res => this.setState({images:res.date.hits})).catch(err => console.log(err))
+    const val = e.target.value;
+    this.setState({[e.target.name]:val}, () =>{
+      if(val === ''){
+        this.setState({images:[]})
+      }else {
+          axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`).then(res => this.setState({images:res.date.hits})).catch(err => console.log(err))
+      }
+
     });
-  }
+  };
 
 onAmountChange = (e, index, value) => this.setState({amount:value})
   render(){
