5433cc43af5496c6a13bff9f2f18439d2663c01f
ghost-fk <fayyad_r22@yahoo.com>
2018-09-11 09:08:47 +0200

    add proptypes to the images

--- src/components/ImagesResults.jsx
+++ src/components/ImagesResults.jsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types'
 
 class ImagesResults extends Component{
   render(){
-    return(<p>test 2</p>)
+    return(
+      <Fragment>
+        test 2
+      </Fragment>)
   }
 }
-
+ImagesResults.propTypes = {
+  images : PropTypes.array.isRequired
+}
 
 export default ImagesResults
