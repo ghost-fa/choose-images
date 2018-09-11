d28e4873752d85bfc607e5ce1285f29e448e8491
ghost-fk <fayyad_r22@yahoo.com>
2018-09-11 09:36:48 +0200

    import icon from material ui and gridlist and gridtile . initilz varible imagesListcontent and add chack for images this props . add if condtion if have image i set the imgesListcontent with criglist and i difiend 3cols and i map the images for each img i render gridtile and if no images just return null and i return imagesListcontent

--- src/components/ImagesResults.jsx
+++ src/components/ImagesResults.jsx
@@ -1,11 +1,43 @@
 import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types'
-
+import PropTypes from 'prop-types';
+import {GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 class ImagesResults extends Component{
+
   render(){
+    let imageListContent;
+    const {images} = this.props
+    if(images){
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile title={img.tags}
+              key={img.id}
+              subtitle={
+                <span>
+                  by <strong>{img.user}</strong>
+                </span>
+              }
+              actionIcon={
+                <IconButton>
+                  <ZoomIn color="white" />
+                </IconButton>
+              }
+              >
+             <img src={img.largImageURL} alt=""/>
+            </GridTile>
+          ))}
+        </GridList>
+      )
+    }else {
+      imageListContent = null;
+    }
     return(
       <Fragment>
-        test 2
+          {imageListContent}
       </Fragment>)
   }
 }
