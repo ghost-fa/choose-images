5584c96d2254670a6922751cb671d94e5dae6b16
ghost-fk <fayyad_r22@yahoo.com>
2018-09-14 14:15:50 +0200

    add deploy

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -20,7 +20,10 @@ class Search extends Component{
       if(val === ''){
         this.setState({images:[]})
       }else {
-          axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`).then(res => this.setState({images:res.date.hits})).catch(err => console.log(err))
+          axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`).then(res => {
+            console.log(res)
+            this.setState({images:res.data.hits})
+          }).catch(err => console.log(err))
       }
 
     });
